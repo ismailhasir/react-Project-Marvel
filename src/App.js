95fb37870f3985cf9ad851bb4ecebb8d079c7176
İsmail HASIR <ismailhasir@gmail.com>
2022-01-23 07:54:27 +0300

    creating the app

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React from "react";
+import styles from "./App.module.css";
+import Cards from "./components/Cards/Cards";
+import Chart from "./components/Chart/Chart";
+import CountryPicker from "./components/CountryPicker/CountryPicker";
+import { fetchData } from "./api";
+
+class App extends React.Component {
+  async componentDidMount() {
+    const data = await fetchData();
+    console.log(data);
+  }
+  render() {
+    return (
+      <div className={styles.container}>
+        <Cards />
+        <Chart />
+        <CountryPicker />
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/CountryPicker/CountryPicker.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const CountryPicker = () => {
+  return <h1>Country Picker</h1>;
+};
+
+export default CountryPicker;
